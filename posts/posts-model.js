870890b1865698c0c1ef1244780c3776fd677ab5
Rory Murray <rory.christian.murray@gmail.com>
2020-02-14 15:59:26 -0500

    Edit model

--- posts/posts-model.js
+++ posts/posts-model.js
@@ -9,12 +9,14 @@ function listPosts() {
     // select * from users
     // return db.select('*').from('users');
     // returns a promise
-    return db('posts');
+    return db('posts as p')
+    .join('users as u','p.user_id', 'u.id').select('p.id as postId', 'p.contents', 'u.username as postedBy')
 }
 
 function listPostById(id) {
     // select * from users where id = id ??
-    return db('posts as p')
-    .join('users as u','p.user_id', 'u.id').select('p.id as postId', 'p.contents', 'u.username as postedBy')
+    return db('posts')
+    .where({ id })
+    .first();
 }
 
